7933cfe1174093440e749872eeae8d92e93e324f
Jonny Gibson <jonny@jgibson.com>
2019-05-27 20:29:48 +0100

    Made start up bash scripts cross platform

--- up.sh
+++ up.sh
@@ -1,5 +1,20 @@
-open "http://localhost:8888/swagger"
-open "http://localhost:8081"
-open "http://localhost:8025"
-open "http://localhost:15672"
+
+case "$OSTYPE" in
+  solaris*) echo "SOLARIS" ;;
+  darwin*)  
+  echo "OSX"
+    open "http://localhost:8888/swagger"
+    open "http://localhost:8081"
+    pen "http://localhost:8025"
+    open "http://localhost:15672" ;; 
+  linux*)   echo "LINUX" ;;
+  bsd*)     echo "BSD" ;;
+  msys*)    
+  echo "WINDOWS"
+  start "http://localhost:8888/swagger"
+    start "http://localhost:8081"
+    start "http://localhost:8025"
+    start "http://localhost:15672" ;;
+  *)        echo "unknown: $OSTYPE" ;;
+esac
 docker-compose up
