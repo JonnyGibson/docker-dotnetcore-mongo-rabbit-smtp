fa945861dcba174c93f0e5d57a51cffbab627b7f
Jonny Gibson <jonathan.gibson@nationwide.co.uk>
2019-05-28 09:38:18 +0100

    reformat shell script

--- up.sh
+++ up.sh
@@ -1,20 +1,21 @@
-
 case "$OSTYPE" in
-  solaris*) echo "SOLARIS" ;;
-  darwin*)  
+solaris*) echo "SOLARIS" ;;
+darwin*)
   echo "OSX"
-    open "http://localhost:8888/swagger"
-    open "http://localhost:8081"
-    pen "http://localhost:8025"
-    open "http://localhost:15672" ;; 
-  linux*)   echo "LINUX" ;;
-  bsd*)     echo "BSD" ;;
-  msys*)    
+  open "http://localhost:8888/swagger"
+  open "http://localhost:8081"
+  open "http://localhost:8025"
+  open "http://localhost:15672"
+  ;;
+linux*) echo "LINUX" ;;
+bsd*) echo "BSD" ;;
+msys*)
   echo "WINDOWS"
   start "http://localhost:8888/swagger"
-    start "http://localhost:8081"
-    start "http://localhost:8025"
-    start "http://localhost:15672" ;;
-  *)        echo "unknown: $OSTYPE" ;;
+  start "http://localhost:8081"
+  start "http://localhost:8025"
+  start "http://localhost:15672"
+  ;;
+*) echo "unknown: $OSTYPE" ;;
 esac
 docker-compose up
